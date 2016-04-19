df572621cb801f7962915d2e22e078de58188fbe
Tulio Carreira <trcarreira@gmail.com>
2016-04-19 04:00:36 -0300

    Tela de cadastro de tarefas

--- src/br/edu/ufabc/tracking2u/App.java
+++ src/br/edu/ufabc/tracking2u/App.java
@@ -1,9 +1,11 @@
 package br.edu.ufabc.tracking2u;
-/*
+
+import br.edu.ufabc.tracking2u.telas.telaPrincipal;
+
 public class App {
 
 	public static void main(String[] args) {
-	}
-
+	System.out.println("teste");
+            new telaPrincipal().setVisible(true);
+        }
 }
-*/
